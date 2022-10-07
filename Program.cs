be50d667eaa39a678ca145e58706087b4918dd3e
OldBeliver <380506338004@yandex.com>
2022-10-07 11:59:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DegreeOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minRandomValue = 0;
+            int maxRandomValue = 2000;
+            int randomValue;
+
+            Random random = new Random();
+            randomValue = random.Next(minRandomValue, maxRandomValue);
+
+            int degree = 2;
+            int calculatedValue = 1;
+            int countDegree = 0;
+
+            while (randomValue >= calculatedValue)
+            {
+                Console.WriteLine($"{randomValue}>{calculatedValue}");
+                countDegree++;
+                calculatedValue *= degree;
+            }
+            
+            Console.WriteLine($"{randomValue} < {calculatedValue}, где {calculatedValue} = 2 в степени {countDegree}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DegreeOfNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DegreeOfNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c999fe32-4ad7-4d70-83b7-1e1727114ff1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
